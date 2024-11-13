d34745574d9ff67c32f40d29157dc55a744032a5
Clément Baconnier <clement@baconnier.ch>
2024-11-13 20:32:36 +0100

    Remove blade-formatter and add nixfmt

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,28 +1,19 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    blade = { "blade-formatter", "prettier" },
+    blade = { "prettier" },
     php = { "pint", "php_cs_fixer" },
     javascript = { "prettier" },
     typescript = { "prettier" },
     typescriptreact = { "prettier" },
     css = { "prettier" },
-    -- html = { "prettier" },
+    html = { "prettier" },
+    nix = { "nixfmt" },
   },
 
   format_on_save = {
     timeout_ms = 500,
     lsp_fallback = true,
-  },
-
-  formatters = {
-    ["blade-formatter"] = {
-      prepend_args = {
-        "--write",
-        "--config",
-        ".prettierrc.json",
-      },
-    },
   },
 }
 
