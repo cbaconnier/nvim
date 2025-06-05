7c3d369bbd9199486c2e99a682818ee786d10f68
Clément Baconnier <clement@baconnier.ch>
2025-06-05 15:13:34 +0200

    Disable j-k for blink

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -38,8 +38,8 @@ return {
         -- ["<CR>"] = { "accept", "fallback" },
         -- ["<C-b>"] = { "scroll_documentation_up", "fallback" },
         -- ["<C-f>"] = { "scroll_documentation_down", "fallback" },
-        ["<C-j>"] = { "select_next", "snippet_forward", "fallback" },
-        ["<C-k>"] = { "select_prev", "snippet_backward", "fallback" },
+        -- ["<C-n>"] = { "select_next", "snippet_forward", "fallback" },
+        -- ["<C-p>"] = { "select_prev", "snippet_backward", "fallback" },
         ["<Tab>"] = {}, -- disable tab
         ["<S-Tab>"] = {}, --disable shift tab
       },
