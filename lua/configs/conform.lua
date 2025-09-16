5f3e9ece8c2fd01a3f378e9e750af8b7a7cd3608
Clément Baconnier <clement@baconnier.ch>
2025-09-16 10:12:08 +0200

    Use php_cs_fixer as fallback

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -9,7 +9,13 @@ local options = {
   formatters_by_ft = {
     lua = { "stylua" },
     blade = { "prettier" },
-    php = { "pint" },
+    php = function()
+      if vim.fn.executable "./vendor/bin/pint" == 1 then
+        return { "pint" }
+      else
+        return { "php_cs_fixer" }
+      end
+    end,
     javascript = { "prettier" },
     json = { "prettier" },
     typescript = { "prettier" },
