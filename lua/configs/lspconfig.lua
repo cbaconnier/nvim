e9ede181a38a15cda185a945bd5e120ca098cdfd
Clément Baconnier <clement@baconnier.ch>
2025-10-16 09:40:54 +0200

    Fix deprecation in lspconfig

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -59,6 +59,5 @@ for name, opts in pairs(servers) do
   opts.on_init = configs.on_init
   opts.on_attach = configs.on_attach
   opts.capabilities = configs.capabilities
-
-  require("lspconfig")[name].setup(opts)
+  vim.lsp.config(name, opts)
 end
