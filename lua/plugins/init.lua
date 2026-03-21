83e86057c92aec6f44e8feb6d2ca2519aedaa9a6
Clément Baconnier <clement@baconnier.ch>
2026-03-21 16:46:44 +0100

    Remove svg from image plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -315,7 +315,7 @@ return {
         backend = "kitty",
         processor = "magick_rock",
         max_height_window_percentage = 50,
-        hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.svg" },
+        hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp" },
       }
     end,
   },
